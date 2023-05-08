5e0f6772d07eb811bb4fb6ab209dee2be7b975b6
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2023-05-08 19:13:43 -0400

    Added Delete Box
    Onto created the update box! (It's currently an empty shell)

--- user_login_test1.php
+++ user_login_test1.php
@@ -30,7 +30,7 @@
    }
 
   div{
-    text-shadow: 2px 2px 1px black;
+
   }
   .createbox{
     color:black;
@@ -42,8 +42,8 @@
     margin-left: auto;
     margin-right: auto;
     position:absolute;
-    top:25%;
-    left:25%;
+    top:10%;
+    left:20%;
   }
 
   .deletebox{
@@ -54,8 +54,10 @@
     width:25%;
     height:33%;
     position:absolute;
-    top:25%;
-    left:62.5%;
+    /*top:25%;
+    left:62.5%;*/
+    top:70%;
+    left:55%;
 
   }
 
@@ -67,25 +69,40 @@
     height:90%;
     width:15%;
     color:white;
-    position: fixed;
-    top:10%;
+    /*float:left*/
+    position: absolute;
+    top:5%;
+    overflow:auto;
 
   }
 
   .checkuserbox{
+    text-shadow: 2px 2px 1px black;
     background-color: tan;
     border:white 3px solid;
-    width:20%;
-    height:25%;
+    width:25%;
+    height:33%;
     position:absolute;
-    top:62.5%;
-    left:25%;
+    /*top:62.5%;
+    left:25%;*/
+    top:40%;
+    right:40%;
   }
 
   .clickbutton{
     cursor:pointer;
   }
 
+
+.updatebox{
+  background-color: lightblue;
+  border: 3px solid blue;
+  color:black;
+  position:absolute;
+  width:25%;
+  height:33%;
+  visibility: hidden;
+}
   #answer{
     color:white;
   }
@@ -97,19 +114,11 @@
 <body>
 
   <?php
-    $servername = "localhost:3306";
-$username = "Laptizzy";
-$password = "Sugarplume";
-
-//$conn = new mysqli($servername,$username,$password);
-//$conn = mysqli_connect("localhost","root","Sugarplume2","testdb3");
 $conn = mysqli_connect("localhost","root");
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-//echo "Connected successfully";
-
 
 $sql = "SELECT username, password FROM testdb3.testtable3";
 $result = $conn->query($sql);
@@ -140,13 +149,11 @@ VALUES
 
 
 <script type="text/javascript">
-
-
 //document.getElementbyId("checksubmit").onclick = function(){loadUp2AJAX()};
 function loadUp1(){
      var username=$('#username').val();
      var password = $('#password').val();
-    // alert("This should work");
+     //alert("This should work");
      $.post("mainuser_login_test.php",'username='+username +'&password=' +password, function(result,status,xhr) {
              if( status.toLowerCase()=="error".toLowerCase() )
              { alert("An Error Occurred.."); }
@@ -173,8 +180,6 @@ function loadUp2(){
          })
          .fail(function(){ alert("something went wrong. Please try again") });
  }
-
-
 /*
 $(function() {
     $( "#checksubmit" ).on( "click", function() {
@@ -182,8 +187,18 @@ $(function() {
     });
 });*/
 
+/*$(function() {
+  $("#deletebut").on("click",function(){
+      var username3 = $("#username3").val();
+      var password3 = $("#password").val();
+
+    $.post()
 
 
+  })
+
+});*/ /*Create 2nd button/function that precedes this that unveils a div
+  in front of the screen and asks user if they're sure. From then THAT button in THAT div will trigger this function*/
 
 function loadUp2AJAX(){
 
@@ -222,7 +237,7 @@ function loadUp2AJAX(){
   <h1>Enter credentials to verify if account exists </h1>
 
 
-<form onsubmit="loadUp1();" action = "mainuser_login_test.php" method = "post" name = "firstbox"> <!--Have the action filled out if you want page redirect to  the php-->
+<form onsubmit="" action = "mainuser_login_test.php" method = "post" name = "firstbox"> <!--Have the action filled out if you want page redirect to  the php-->
 <div> Add Username: </div>
 <input id = "username" type = "text" name = "username"/>
 <br/>
@@ -230,13 +245,7 @@ function loadUp2AJAX(){
 <input id = "password" type = "password" name = "password"/>
 
 <input class = "clickbutton" type = "submit" value = "Submit" />
-
-
 </form>
-
-
-
-
 </div>
 
 <!--<form name = "box2" method = "get" onsubmit="loadUp2AJAX();" action = "">
@@ -248,12 +257,12 @@ function loadUp2AJAX(){
 </form> --> <!--<form action = "checkusername.php" method="get" target = "_blank">  -->
 
 <div class = "deletebox">
-<form name = "deleteform" method = "post" action = "" onsubmit="">
+<form name = "deleteform" method = "post" action = "deleteuser.php" onsubmit="">
 <h2> TIME TO DELETE?</h2>
 
-<div>Username:</div> <input type = "text" id ="username3" />
+<div>Username:</div> <input type = "text" name = "username3" id ="username3" />
 
-<div>Password:</div> <input type = "password" id = "password3" />
+<div>Password:</div> <input type = "password" name = "password3" id = "password3" />
 <input class = "clickbutton" type = "submit" value = "DELETE" id = "deletebut"/>
 </form>
 </div>
@@ -267,12 +276,12 @@ function loadUp2AJAX(){
 </div>
 
 
+<div class = "updatebox">
+<h2>Update username or password?  </h2>
+<input type = "text" />
+
+</div>
+
 </div> <!--For .overall div -- >
-
-
-
-
-
 </body>
-
 </html>
