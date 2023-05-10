3b0bc4ffca6295f3db0dc8ffa962bd1ca2d41fa1
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2023-05-10 15:25:09 -0400

    Update user_update.php
    Added precautions for empty input and account existence verification.

--- user_update.php
+++ user_update.php
@@ -15,6 +15,22 @@ $newuser = $_REQUEST['newuser'];
 $oldpass = $_REQUEST['oldpass'];
 $newpass = $_REQUEST['newpass'];
 
+if($olduser === "" || $newuser === "" || $oldpass === "" || $newpass === ""){
+  echo "Missing identifications; Check again";
+  exit;
+}
+
+
+$sql = "SELECT username FROM testdb3.testtable3 WHERE username = '$olduser'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows <= 0){
+  echo "Username/Account does not exist.";
+  exit;
+}
+
+
 $sql = "UPDATE testdb3.testtable3 SET username = '$newuser',password = '$newpass' WHERE username = '$olduser' AND password = '$oldpass'";
 
 if(mysqli_query($conn,$sql)){
