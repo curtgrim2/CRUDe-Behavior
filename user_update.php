d412b9c437fa19bad4321c03934350c26e2dfc7a
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2023-05-14 11:03:46 -0400

    Added checkbox options + logic.

--- user_update.php
+++ user_update.php
@@ -14,6 +14,7 @@ $olduser = $_REQUEST['olduser'];
 $newuser = $_REQUEST['newuser'];
 $oldpass = $_REQUEST['oldpass'];
 $newpass = $_REQUEST['newpass'];
+$nouserissue = $_REQUEST['nouserissue'];
 
 if($olduser === "" || $newuser === "" || $oldpass === "" || $newpass === ""){
   echo "Missing identifications; Check again";
@@ -21,33 +22,35 @@ if($olduser === "" || $newuser === "" || $oldpass === "" || $newpass === ""){
 }
 
 
-$sql = "SELECT username FROM testdb3.testtable3 WHERE username = '$olduser'";
+$sql = "SELECT username FROM testdb3.testtable3 WHERE username = '$olduser' AND password = '$oldpass'";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows <= 0){
-  echo "Username/Account does not exist.";
+  echo "Account does not exist. Username and/or Password incorrect";
   exit;
 }
 
-
-//Now lets check if the user name already exists.
-
 $sql = "SELECT username FROM testdb3.testtable3 WHERE username = '$newuser'";
-
-
 $result = $conn->query($sql);
 
 
-if ($result->num_rows > 0 ) {
+if($nouserissue == true){
+
+if ($result->num_rows > 1 ) {
   echo "Username already exists. Please try a different one!";
 exit;
 }
 
+}
 
+else{
 
-
-
+  if ($result->num_rows > 0 ) {
+    echo "Username already exists. Please try a different one!";
+  exit;
+    }
+}
 
 $sql = "UPDATE testdb3.testtable3 SET username = '$newuser',password = '$newpass' WHERE username = '$olduser' AND password = '$oldpass'";
 
@@ -55,11 +58,15 @@ if(mysqli_query($conn,$sql)){
 
   echo "Account has been updated";
 
-}
+  }
 
-else {
-  echo "An error has occured in inputting information. Try Again.";
-}
+  else {
+    echo "An error has occured in inputting information. Try Again.";
+  }
+
+
+
+
 
 mysqli_close($conn);
 ?>
